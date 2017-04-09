558ecb3093afcbc23cd719201816bb9d4d872528
shery15 <382254994@qq.com>
2017-04-09 17:47:52 +0800

    update

--- tick.js
+++ tick.js
@@ -1,6 +1,11 @@
-function tick(...deadline) {
-  var end = new Date(...deadline);
-  var now = new Date();
-  var duration = end.getTime() - now.getTime();
-  
+function tick(year,month = 1,day = 1,hour = 0,minute = 0,second = 0) {
+  const end = new Date(year, month - 1, day, hour, minute, second);
+  const now = new Date();
+  const duration = end.getTime() - now.getTime();
+  const rday = Math.floor(duration / 1000 / 60 / 60 / 24);
+  const rhour = Math.floor(duration / 1000 / 60 / 60 % 24);
+  const rmin = Math.floor(duration / 1000 / 60 % 60);
+  const rsecs = Math.floor(duration / 1000 % 60);
+  const str = `还剩${rday}天${rhour}小时${rmin}分钟${rsecs}秒`;
+  console.log(str);
 }
