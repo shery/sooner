75fe1c32c1fb6a75a16ed71a734ab5658d79dcd0
shery15 <382254994@qq.com>
2017-04-15 22:04:10 +0800

    update

--- tick.js
+++ tick.js
@@ -6,13 +6,8 @@ function tick(year, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
   const rhour = Math.floor(duration / 1000 / 60 / 60 % 24);
   const rmin = Math.floor(duration / 1000 / 60 % 60);
   const rsecs = Math.floor(duration / 1000 % 60);
-  const str = `还剩${rday}天${rhour}小时${rmin}分钟${rsecs}秒`;
-  console.log(str);
+  return { rday, rhour, rmin, rsecs }
 }
-setInterval(() => {
-    tick(2017, 5, 4);
-}, 1000);
-
 
 if (typeof module !== "undefined")
 module.exports = tick;
