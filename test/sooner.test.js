418f0457b124cdddd860484047eec4c3f57584c3
shery15 <382254994@qq.com>
2017-04-16 00:34:52 +0800

    update test

--- test/sooner.test.js
+++ test/sooner.test.js
@@ -1,8 +1,2 @@
 import sooner from '../sooner.js';
-import { expect } from 'chai';
-
-describe('倒计时函数测试', function() {
-  it('输出格式验证', function() {
-    expect(sooner(2017, 4, 16)).to.be.an('object');
-  });
-});
+import test from 'ava';
