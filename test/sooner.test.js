94733c59e5a79e8b582f9c9f6655c14d911ea48b
shery15 <382254994@qq.com>
2017-04-18 23:44:58 +0800

    update test ava

--- test/sooner.test.js
+++ test/sooner.test.js
@@ -4,3 +4,19 @@ import test from 'ava';
 test('输出格式验证', t => {
   t.is(typeof sooner(2017,4,20), 'object');
 });
+
+test('输出含有天', t => {
+  t.truthy(Object.keys(sooner(2017,4,20)).includes('rday'));
+});
+
+test('输出含有小时', t => {
+  t.truthy(Object.keys(sooner(2017,4,20)).includes('rhour'));
+});
+
+test('输出含有分钟', t => {
+  t.truthy(Object.keys(sooner(2017,4,20)).includes('rmin'));
+});
+
+test('输出含有秒', t => {
+  t.truthy(Object.keys(sooner(2017,4,20)).includes('rsecs'));
+});
