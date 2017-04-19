dcc0889458cc2e1e809d52ee94cfe87c26a5762d
shery15 <382254994@qq.com>
2017-04-19 23:08:17 +0800

    update

--- sooner.js
+++ sooner.js
@@ -3,9 +3,9 @@ function sooner(year, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
   const now = new Date();
   const duration = end.getTime() - now.getTime();
   const rday = Math.floor(duration / 1000 / 60 / 60 / 24);
-  const rhour = Math.floor(duration / 1000 / 60 / 60 % 24);
-  const rmin = Math.floor(duration / 1000 / 60 % 60);
-  const rsecs = Math.floor(duration / 1000 % 60);
+  const rhour = Math.floor((duration / 1000 / 60 / 60) % 24);
+  const rmin = Math.floor((duration / 1000 / 60) % 60);
+  const rsecs = Math.floor((duration / 1000 %) 60);
   return { rday, rhour, rmin, rsecs }
 }
 
