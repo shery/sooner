1253df66237e3229e10637a617e7ce171959a667
shery15 <382254994@qq.com>
2017-05-11 22:21:24 +0800

    fix bug

--- sooner.js
+++ sooner.js
@@ -5,7 +5,7 @@ function sooner(year, month = 1, day = 1, hour = 0, minute = 0, second = 0) {
   const rday = Math.floor(duration / 1000 / 60 / 60 / 24);
   const rhour = Math.floor((duration / 1000 / 60 / 60) % 24);
   const rmin = Math.floor((duration / 1000 / 60) % 60);
-  const rsecs = Math.floor((duration / 1000 %) 60);
+  const rsecs = Math.floor((duration / 1000 ) % 60);
   return { rday, rhour, rmin, rsecs }
 }
 
